fe9fc05a3d486879872f4d20d949f36a3fbea540
Shamimul Shimul <shamimrshimul0403@gmail.com>
2026-02-02 20:40:09 +0600

    Fix SQL syntax by replacing single quotes with backticks

--- project.sql
+++ project.sql
@@ -27,17 +27,18 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE 'admin' (
-  'id' varchar(10) NOT NULL,
-  'name' varchar(11) NOT NULL,
-  'pass' varchar(10) NOT NULL
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` varchar(10) NOT NULL,
+  `name` varchar(11) NOT NULL,
+  `pass` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO 'admin' ('id', 'name', 'pass') VALUES
+INSERT INTO `admin` (`id`, `name`, `pass`) VALUES
 ('1', 'shimul', '111');
 
 -- --------------------------------------------------------
@@ -46,18 +47,19 @@ INSERT INTO 'admin' ('id', 'name', 'pass') VALUES
 -- Table structure for table `history`
 --
 
-CREATE TABLE 'history' (
-  'hid' int(10) NOT NULL,
-  'uid' int(10) NOT NULL,
-  'phone' int(11) NOT NULL,
-  'bill' int(10) NOT NULL
+DROP TABLE IF EXISTS `history`;
+CREATE TABLE `history` (
+  `hid` int(10) NOT NULL,
+  `uid` int(10) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `bill` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `history`
 --
 
-INSERT INTO 'history' ('hid', 'uid', 'phone', 'bill') VALUES
+INSERT INTO `history` (`hid`, `uid`, `phone`, `bill`) VALUES
 (23, 26, 1711111111, 30),
 (24, 26, 1711111111, 0);
 
@@ -67,14 +69,15 @@ INSERT INTO 'history' ('hid', 'uid', 'phone', 'bill') VALUES
 -- Table structure for table `reserve`
 --
 
-CREATE TABLE 'reserve' (
-  'rid' int(10) NOT NULL,
-  'phone' int(11) NOT NULL,
-  'slot' int(1) NOT NULL,
-  'reserveTime' time NOT NULL,
-  'unreserveTime' time NOT NULL,
-  'timeDef' int(10) NOT NULL,
-  'bill' int(10) NOT NULL
+DROP TABLE IF EXISTS `reserve`;
+CREATE TABLE `reserve` (
+  `rid` int(10) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `slot` int(1) NOT NULL,
+  `reserveTime` time NOT NULL,
+  `unreserveTime` time NOT NULL,
+  `timeDef` int(10) NOT NULL,
+  `bill` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -83,18 +86,19 @@ CREATE TABLE 'reserve' (
 -- Table structure for table `user`
 --
 
-CREATE TABLE 'user' (
-  'uid' int(10) NOT NULL,
-  'name' varchar(40) NOT NULL,
-  'phone' int(11) NOT NULL,
-  'password' varchar(40) NOT NULL
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int(10) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `password` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table 'user'
+-- Dumping data for table `user`
 --
 
-INSERT INTO 'user' ('uid', 'name', 'phone', 'password') VALUES
+INSERT INTO `user` (`uid`, `name`, `phone`, `password`) VALUES
 (26, 'shimul', 1711111111, 'test'),
 (27, 'test', 1711111112, 'PASS123');
 
@@ -105,30 +109,30 @@ INSERT INTO 'user' ('uid', 'name', 'phone', 'password') VALUES
 --
 -- Indexes for table `admin`
 --
-ALTER TABLE 'admin'
-  ADD PRIMARY KEY ('id');
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `history`
 --
-ALTER TABLE 'history'
-  ADD PRIMARY KEY ('hid'),
-  ADD KEY 't1' ('uid');
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`hid`),
+  ADD KEY `t1` (`uid`);
 
 --
 -- Indexes for table `reserve`
 --
-ALTER TABLE 'reserve'
-  ADD PRIMARY KEY ('rid'),
-  ADD KEY 'slot' ('slot'),
-  ADD KEY 't2' ('phone');
+ALTER TABLE `reserve`
+  ADD PRIMARY KEY (`rid`),
+  ADD KEY `slot` (`slot`),
+  ADD KEY `t2` (`phone`);
 
 --
 -- Indexes for table `user`
 --
-ALTER TABLE 'user'
-  ADD PRIMARY KEY ('uid'),
-  ADD UNIQUE KEY 'phone' ('phone');
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `phone` (`phone`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -137,20 +141,20 @@ ALTER TABLE 'user'
 --
 -- AUTO_INCREMENT for table `history`
 --
-ALTER TABLE 'history'
-  MODIFY 'hid' int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+ALTER TABLE `history`
+  MODIFY `hid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `reserve`
 --
-ALTER TABLE 'reserve'
-  MODIFY 'rid' int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+ALTER TABLE `reserve`
+  MODIFY `rid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE 'user'
-  MODIFY 'uid' int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+ALTER TABLE `user`
+  MODIFY `uid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Constraints for dumped tables
@@ -159,14 +163,14 @@ ALTER TABLE 'user'
 --
 -- Constraints for table `history`
 --
-ALTER TABLE 'history'
-  ADD CONSTRAINT 't1' FOREIGN KEY ('uid') REFERENCES 'user' ('uid') ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `history`
+  ADD CONSTRAINT `t1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reserve`
 --
-ALTER TABLE 'reserve'
-  ADD CONSTRAINT 't2' FOREIGN KEY ('phone') REFERENCES 'user' ('phone') ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `reserve`
+  ADD CONSTRAINT `t2` FOREIGN KEY (`phone`) REFERENCES `user` (`phone`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
